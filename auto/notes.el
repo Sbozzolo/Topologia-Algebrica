845e4a50021fbbbcd681528268bcbb1dcfa6e67e
Gabriele Bozzola <sbozzolator@gmail.com>
2016-10-27 16:12:09 +0200

    Cambiare ordine

--- auto/notes.el
+++ auto/notes.el
@@ -5,11 +5,19 @@
                      '(("scrbook" "10pt" "twoside=false" "x11names")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "italian") ("fontenc" "T1") ("hyperref" "hidelinks")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "titlepage"
     "listofsymb"
     "images/torus_generators"
+    "images/Tetrahedron"
     "images/Klein_bottle"
     "scrbook"
     "scrbook10"
@@ -47,18 +55,19 @@
     "M"
     "N"
     "Z"
+    "me"
     "homoto"
     "incl"
     "svgwidth")
    (LaTeX-add-labels
+    "fig:lez3:homotopy"
+    "fig:lez3:clifford_torus"
+    "fig:lez3:torus_generators"
     "fig:lez1:1_standard_simplex"
     "fig:lez1:standard_simplexes"
     "fig:lez1:1_standard_simplex_with_arc"
     "fig:lez1:standard_simplex_faces"
     "fig:lez1:sigma"
-    "fig:lez3:homotopy"
-    "fig:lez3:clifford_torus"
-    "fig:lez3:torus_generators"
     "fig:lez3:proof_homo"
     "fig:lez3:sketch_of_proof"
     "fig:lez3:klein_bottle"
@@ -70,6 +79,7 @@
     "osservation"
     "corollary"
     "definition"
+    "example"
     "exercise"
     "proof")
    (LaTeX-add-index-entries
@@ -97,9 +107,19 @@
     "Ricoprimento"
     "Insieme compatto"
     "Omeomorfismo"
+    "Laccio"
+    "Spazio topologico puntato"
+    "Relazione di omotopia"
+    "Omotopia! \\vedi{Relazione di omotopia}"
+    "Gruppo fondamentale"
+    "Cammino composto"
+    "Giunzione!\\vedi{Cammino composto}"
+    "Spazio contraibile"
+    "Semplicemente connesso"
+    "Teorema di Seifert–van Kampen"
+    "Genere"
     "Arco"
     "Spazio connesso per archi"
-    "Gruppo fondamentale"
     "Gruppi di omotopia superiore"
     "Inclusione"
     "Simplesso standard"
@@ -112,17 +132,8 @@
     "$ k $-ciclo"
     "Elementi omologhi"
     "Grado"
+    "Teorema fondamentale degli omomorfismi"
     "Gruppo derivato"
-    "Laccio"
-    "Spazio topologico puntato"
-    "Relazione di omotopia"
-    "Omotopia! \\vedi{Relazione di omotopia}"
-    "Cammino composto"
-    "Giunzione!\\vedi{Cammino composto}"
-    "Spazio contraibile"
-    "Semplicemente connesso"
-    "Teorema di Seifert–van Kampen"
-    "Genere"
     "Retratto di deformazione"
     "Retrazione"
     "Teorema del punto fisso"
