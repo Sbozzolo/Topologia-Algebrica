83f2a87411ab2526dc98e2c31755570b157e2587
Gabriele Bozzola <sbozzolator@gmail.com>
2017-01-12 12:43:52 +0100

    Fixes

--- auto/cellular.el
+++ auto/cellular.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
  "cellular"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "images/Dodecahedron")
@@ -28,7 +28,7 @@
     "Spazio a omologia razionale ! \\vedi{Spazio dodecaedrico}"
     "Rappresentazione spinoriale di SO(3)"
     "Caratteristica di Eulero di un CW-complesso"
-    "Coppia Buona"
+    "Coppia buona"
     "Omologia cellulare"
     "Teorema di struttura per gruppi abeliani liberi finitamente generati"
     "Successione di Mayer-Vietoris"
